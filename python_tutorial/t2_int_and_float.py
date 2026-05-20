c0058a98bacbd73b60976752f995eff0fbbd9bc0
Usama Mahboob <usa.mahboob@gmail.com>
2026-05-20 21:55:41 +0100

    Added lesson on int and float.

---
+++ python_tutorial/t2_int_and_float.py
@@ -0,0 +1,46 @@
+
+num = 3
+print(type(num))  # Output: <class 'int'>
+
+pi = 3.14
+print(type(pi))  # Output: <class 'float'>
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)   # division (3 / 2) = 1.5
+print(3 // 2)  # floor division (3 / 2) = 1
+print(3 ** 2)  # exponent
+print(3 % 2)   # modulus
+print(2 % 2)   # modulus (2 % 2) = 0
+print("\n")
+# Type conversion
+num_str = "123"
+num_int = int(num_str)  # Convert string to integer
+num_float = float(num_str)  # Convert string to float
+print(num_int)  # Output: 123
+print(num_float)  # Output: 123.0
+print(type(num_int))   # Output: <class 'int'>
+print(type(num_float)) # Output: <class 'float'>
+
+print(3 * 2 + 1)  # Output: 7
+print(3 * (2 + 1))  # Output: 9
+
+num = 5
+num += 2  # Equivalent to num = num + 2
+print(num)  # Output: 7
+
+print(abs(-3))  # Output: 3
+print(round(3.14159, 2))  # Output: 3.14
+print(round(3.14159))  # Output: 3
+print(round(3.75, 1))  # Output: 3.8
+
+num_1 = 3
+num_2 = 4
+print(num_1 > num_2)  # Output: False
+print(num_1 < num_2)  # Output: True
+print(num_1 == num_2)  # Output: False
+print(num_1 != num_2)  # Output: True
+print(num_1 >= num_2)  # Output: False
+print(num_1 <= num_2)  # Output: True
+
